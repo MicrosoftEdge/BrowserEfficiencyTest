87ab7e19794c680011ed7aeb24e2bfcb059dc964
Chris Hammer <ChammerMSFT@Outlook.com>
2016-06-15 11:06:28 -0700

    Reduced the constructors to just two
    Removed the extra StartWPR and StopWPR overloads
    
    Add standard error redirection to the RunWPR API.

--- TestingPower/AutomateWPR.cs
+++ TestingPower/AutomateWPR.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TestingPower
 {
@@ -19,92 +20,50 @@ namespace TestingPower
     {
         private string wprExePath;
         private string wprpFile;
-        private string etlFileName;
+
+        public string WprRecordingProfile
+        {
+            get
+            {
+                return wprpFile;
+            }
+            set
+            {
+                this.wprpFile = value;
+            }
+        }
 
         /// <summary>
         /// Initializes a new instance of the AutomateWPR class with default settings.
         /// </summary>
         public AutomateWPR()
         {
-            this.wprExePath = @"C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\wpr.exe";
-            this.etlFileName = "BrowserPower.etl";
+            this.wprExePath = @"C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\wpr.exe";            
             this.wprpFile = ""; // need a default wprp file
-        }
-        
-        /// <summary>
-        /// Initializes a new instance of the WprWrapper class with the specified wprp file.
-        /// </summary>
-        /// <param name="wprpFile">The wprp file to use for tracing.</param>
-        public AutomateWPR(string wprpFile)
-        {
-            this.wprExePath = @"C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\wpr.exe";
-            this.etlFileName = "BrowserPower.etl";
-            this.wprpFile = wprpFile;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the WprWrapper class with the specified wprp file and WPR.exe path.
-        /// </summary>
-        /// <param name="wprpFile">The wprp file to use for tracing.</param>
-        /// <param name="etlFileName">The ETL file to use for saving the trace session to.</param>
-        public AutomateWPR(string wprpFile, string etlFileName)
-        {
-            this.wprExePath = @"C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\wpr.exe";
-            this.etlFileName = "BrowserPower.etl";
-            this.wprpFile = wprpFile;
         }
 
         /// <summary>
         /// Initializes a new instance of the WprWrapper class with the specified wprp file, WPR.exe path, and ETL file name.
         /// </summary>        
         /// <param name="wprpFile">The wprp file to use for tracing.</param>
-        /// <param name="etlFileName">The ETL file to use for saving the trace session to.</param>
         /// <param name="wprPath">The WPR exe path and file name to use for controlling the WPR tracing.</param>
-        public AutomateWPR(string wprpFile, string etlFileName, string wprPath)
+        public AutomateWPR(string wprpFile, string wprPath = @"C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\wpr.exe")
         {
             this.wprExePath = wprPath;
-            this.etlFileName = etlFileName;
             this.wprpFile = wprpFile;
-        }
-
-        /// <summary>
-        /// Starts a WPR recording session.
-        /// </summary>
-        /// <returns>0 if the trace session was started successfully.</returns>
-        public int StartWPR()
-        {
-            int _retVal = -1;
-            string _commandLine = "-start " + this.wprpFile;
-
-            _retVal = this.RunWpr(_commandLine);
-            return _retVal;
         }
 
         /// <summary>
         /// Starts a WPR recording session using the specified wprp file.
         /// </summary>
-        /// <param name="wprpFile">The name of the wprp file to start the WPR tracing session with.</param>
         /// <returns>0 if the trace session was started successfully.</returns>
-        public int StartWPR(string wprpFile)
+        public int StartWPR()
         {
             int _retVal = -1;
+            
             string _commandLine = "-start " + this.wprpFile;
 
             _retVal = this.RunWpr(_commandLine);
-            return _retVal;
-        }
-
-        /// <summary>
-        /// Stops the currently running WPR recording and saves it to an ETL file.
-        /// </summary>
-        /// <returns>0 if the trace session was stopped successfully.</returns>
-        public int WprStop()
-        {
-            int _retVal = -1;
-            string _commandLine = "-stop " + this.etlFileName;
-
-            _retVal = this.RunWpr(_commandLine);
-
             return _retVal;
         }
 
@@ -113,9 +72,10 @@ namespace TestingPower
         /// </summary>
         /// <param name="etlFileName">The ETL file name to save the recording to.</param>
         /// <returns></returns>
-        public int WprStop(string etlFileName)
+        public int StopWPR(string etlFileName = "BrowserPower.etl")
         {
             int _retVal = -1;
+            
             string _commandLine = "-stop " + etlFileName;
 
             _retVal = this.RunWpr(_commandLine);
@@ -127,19 +87,33 @@ namespace TestingPower
         private int RunWpr(string cmdLine)
         {
             int _retVal = 1;
+            string errorOutput = "";            
 
             string _wprExe = System.IO.Path.Combine(this.wprExePath, "wpr.exe");
-
+            
             try
             {
                 ProcessStartInfo processInfo = new ProcessStartInfo(_wprExe);
                 processInfo.Arguments = cmdLine;                                
                 processInfo.UseShellExecute = false;
+                processInfo.RedirectStandardError = true;                
 
                 Process commandProcess = new Process();
                 commandProcess.StartInfo = processInfo;
                 commandProcess.Start();
+                
+                // capture any error output. We'll use this to throw an exception.
+                errorOutput = commandProcess.StandardError.ReadToEnd();
+
                 commandProcess.WaitForExit();
+                
+                // output the standard error to the console window. The standard output is routed to the console window by default.
+                Console.WriteLine(errorOutput);                
+                                
+                if (!String.IsNullOrEmpty(errorOutput))
+                {
+                    throw new Exception(errorOutput.ToString());
+                }
 
                 _retVal = 0;
             }
@@ -147,6 +121,7 @@ namespace TestingPower
             {
                 Console.WriteLine("Exception trying to run wpr.exe!");
                 Console.WriteLine(e.Message);
+                
                 _retVal = -1;
             }
 
