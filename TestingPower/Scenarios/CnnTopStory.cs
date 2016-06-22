bdf496fd96b435210a9cfc37daac35027b14759c
Chris Hammer <ChammerMSFT@Outlook.com>
2016-06-22 00:13:44 -0700

    Fixed cnn scenario to find top story element consistently.
    Added TechRadar scenario
    
    Added error check to the scenario remaining time duration calculation.
    
    Added cnn and techRadar scenarios to the 'all' scenario

--- TestingPower/Scenarios/CnnTopStory.cs
+++ TestingPower/Scenarios/CnnTopStory.cs
@@ -11,7 +11,9 @@ namespace TestingPower
         public CnnTopStory()
         {
             Name = "cnn";
-            Duration = 60;
+
+            // Using 80 as sometimes Chrome takes just over 70 seconds to run            
+            Duration = 80;
         }
 
         public override void Run(RemoteWebDriver driver, string browser, List<UserInfo> logins)
@@ -22,14 +24,13 @@ namespace TestingPower
             Thread.Sleep(5000); 
 
             // Get the element that contains the headline story
-            var headlineElement = driver.FindElementByClassName("zn-banner");
-            var aRefElement = headlineElement.FindElement(By.TagName("a"));
+            var headlineElement = driver.FindElementByClassName("js-screaming-banner");
 
-            // Get focus on the headline story link 
-            aRefElement.SendKeys(String.Empty);
+            // Get focus on the headline story link
+            headlineElement.SendKeys(String.Empty);
 
             // Open the link to the headline story
-            aRefElement.SendKeys(Keys.Enter);
+            headlineElement.SendKeys(Keys.Enter);
 
             // And let that load
             Thread.Sleep(2 * 1000);
