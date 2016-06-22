860251ef4efe97d9630b6e0a96a31fd3f5a777b5
Adalberto Foresti <aforesti@microsoft.com>
2016-06-22 12:24:30 -0700

    Adding License and copyright headers. Removed unnecessary usings.

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ PowerTest is a [Selenium WebDriver](http://docs.seleniumhq.org/) based web brows
 
 ## Coding Style
 This project follows the [dotnet/corefx C# Coding Style](https://github.com/dotnet/corefx/blob/master/Documentation/coding-guidelines/coding-style.md).
+
+## Code of Conduct
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
--- TestingPower/AutomateWPR.cs
+++ TestingPower/AutomateWPR.cs
@@ -1,22 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿//--------------------------------------------------------------
+//
+// Microsoft Edge Power Test
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files(the ""Software""),
+// to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+// OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//--------------------------------------------------------------
 
-namespace TestingPower
-{
-    /// <summary>
-    /// Provides Windows tracing controls using Windows Performance Recorder (WPR).
-    /// By default AutomateWPR assumes WPR has been installed on the test machine as part of the Windows Performance Toolkit.
-    /// Other WPR.exe can optionally be specified by passing the path to the WPR.exe location during instantiation.
-    ///
-    /// NOTE: TestingPower.exe must be run elevated in order to allow control of WPR.exe.
-    /// 
-    /// </summary>
-    class AutomateWPR
+﻿using System;
+using System.Diagnostics;
+
+namespace TestingPower {
+   /// <summary>
+   /// Provides Windows tracing controls using Windows Performance Recorder (WPR).
+   /// By default AutomateWPR assumes WPR has been installed on the test machine as part of the Windows Performance Toolkit.
+   /// Other WPR.exe can optionally be specified by passing the path to the WPR.exe location during instantiation.
+   ///
+   /// NOTE: TestingPower.exe must be run elevated in order to allow control of WPR.exe.
+   /// 
+   /// </summary>
+   class AutomateWPR
     {
         private string wprExePath;
         private string wprpFile;
