77ab2a2999ef3724fecc2458702ac00dfd595e51
Brandon Heenan <brheenan>
2016-08-30 13:25:10 -0700

    Skips over exceptions
    When we encounter an exception while running a test, we put Elevator and
    the browser back in their beginning states and skip over to the next
    iteration/browser. The exception is still written to the console

--- TestingPower/ScenarioRunner.cs
+++ TestingPower/ScenarioRunner.cs
@@ -40,7 +40,7 @@ namespace TestingPower
     /// Executes automated scenarios on selected web browsers using WebDriver
     /// </summary>
     internal class ScenarioRunner
-    {                 
+    {
         private bool _doWarmup;
         private int _loops;
         private int _iterations;
@@ -48,10 +48,10 @@ namespace TestingPower
         private bool _usingTraceController;
         private string _etlPath;
 
-        private List<Scenario> _scenarios = new List<Scenario>();        
+        private List<Scenario> _scenarios = new List<Scenario>();
         private List<string> _browsers = new List<string>();
         private List<UserInfo> _logins;
-        
+
         private string _scenarioName;
         private int _e3RefreshDelaySeconds;
 
@@ -149,58 +149,85 @@ namespace TestingPower
                 {
                     foreach (string browser in _browsers)
                     {
+                        bool hadToCancelPass = false;
+
                         elevatorClient.SendControllerMessageAsync($"{Elevator.Commands.START_BROWSER} {browser} ITERATION {iteration} SCENARIO_NAME {_scenarioName}").Wait();
 
                         Console.WriteLine("[{0}] - Launching Browser Driver {1} -", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), browser);
 
                         using (var driver = RemoteWebDriverExtension.CreateDriverAndMaximize(browser))
                         {
-                            Stopwatch watch = Stopwatch.StartNew();
-                            bool isFirstScenario = true;
-
-                            foreach (var scenario in _scenarios)
+                            try
                             {
-                                // We want every scenario to take the same amount of time total, even if there are changes in
-                                // how long pages take to load. The biggest reason for this is so that you can measure energy
-                                // or power and their ratios will be the same either way.
-                                // So start by getting the current time.
-                                var startTime = watch.Elapsed;
+                                Stopwatch watch = Stopwatch.StartNew();
+                                bool isFirstScenario = true;
 
-                                // The first scenario naviagates in the browser's new tab / welcome page.
-                                // After that, scenarios open in their own tabs
-                                if (!isFirstScenario)
+                                foreach (var scenario in _scenarios)
                                 {
-                                    driver.CreateNewTab(browser);
+                                    // We want every scenario to take the same amount of time total, even if there are changes in
+                                    // how long pages take to load. The biggest reason for this is so that you can measure energy
+                                    // or power and their ratios will be the same either way.
+                                    // So start by getting the current time.
+                                    var startTime = watch.Elapsed;
+
+                                    // The first scenario naviagates in the browser's new tab / welcome page.
+                                    // After that, scenarios open in their own tabs
+                                    if (!isFirstScenario)
+                                    {
+                                        driver.CreateNewTab(browser);
+                                    }
+                                    else
+                                    {
+                                        isFirstScenario = false;
+                                    }
+
+                                    Console.WriteLine("[{0}] - Executing - Iteration: {1}  Browser: {2}  Scenario: {3}.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), iteration, browser, scenario.Name);
+
+                                    // Here, control is handed to the scenario to navigate, and do whatever it wants
+                                    scenario.Run(driver, browser, _logins);
+
+                                    // When we get control back, we sleep for the remaining time for the scenario. This ensures
+                                    // the total time for a scenario is always the same
+                                    var runTime = watch.Elapsed.Subtract(startTime);
+                                    var timeLeft = TimeSpan.FromSeconds(scenario.Duration).Subtract(runTime);
+                                    if (timeLeft < TimeSpan.FromSeconds(0))
+                                    {
+                                        // Of course it's possible we don't get control back until after we were supposed to
+                                        // continue to the next scenario. In that case, invalidate the run by throwing.
+                                        throw new Exception(string.Format("Scenario ran longer than expected! The browser ran for {0}s. The timeout for this scenario is {1}s.", runTime.TotalSeconds, scenario.Duration));
+                                    }
+
+                                    Console.WriteLine("[{0}] - Completed - Iteration: {1}  Browser: {2}  Scenario: {3}. Scenario ran for {4} seconds.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), iteration, browser, scenario.Name, runTime.TotalSeconds);
+
+                                    Thread.Sleep(timeLeft);
                                 }
-                                else
-                                {
-                                    isFirstScenario = false;
-                                }
 
-                                Console.WriteLine("[{0}] - Executing - Iteration: {1}  Browser: {2}  Scenario: {3}.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), iteration, browser, scenario.Name);
+                                Console.WriteLine("[{0}] - Completed Browser: {1}  Iteration: {2} ", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), browser, iteration);
 
-                                // Here, control is handed to the scenario to navigate, and do whatever it wants
-                                scenario.Run(driver, browser, _logins);
+                                driver.CloseAllTabs(browser);
 
-                                // When we get control back, we sleep for the remaining time for the scenario. This ensures
-                                // the total time for a scenario is always the same
-                                var runTime = watch.Elapsed.Subtract(startTime);
-                                var timeLeft = TimeSpan.FromSeconds(scenario.Duration).Subtract(runTime);
-                                if (timeLeft < TimeSpan.FromSeconds(0))
-                                {
-                                    // Of course it's possible we don't get control back until after we were supposed to
-                                    // continue to the next scenario. In that case, invalidate the run by throwing.
-                                    throw new Exception(string.Format("Scenario ran longer than expected! The browser ran for {0}s. The timeout for this scenario is {1}s.", runTime.TotalSeconds, scenario.Duration));
-                                }
-
-                                Console.WriteLine("[{0}] - Completed - Iteration: {1}  Browser: {2}  Scenario: {3}. Scenario ran for {4} seconds.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), iteration, browser, scenario.Name, runTime.TotalSeconds);
-
-                                Thread.Sleep(timeLeft);
                             }
-
-                            Console.WriteLine("[{0}] - Completed Browser: {1}  Iteration: {2} ", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), browser, iteration);
-
-                            driver.CloseAllTabs(browser);
+                            catch (Exception ex)
+                            {
+                                // If something goes wrong and we get an exception halfway through the scenario, we clean up
+                                // and put everything back into a state where we can start the next iteration.
+                                elevatorClient.SendControllerMessageAsync(Elevator.Commands.CANCEL_PASS);
+                                try
+                                {
+                                    driver.CloseAllTabs(browser);
+                                }
+                                catch
+                                { }
+                                hadToCancelPass = true;
+                                Console.WriteLine("--EXCEPTION----------------------------------------------");
+                                Console.WriteLine("Caught exception while trying to run scenario. Exception:");
+                                Console.WriteLine(ex);
+                                if (_usingTraceController)
+                                {
+                                    Console.WriteLine("Trace has been discarded");
+                                }
+                                Console.WriteLine("---------------------------------------------------------");
+                            }
                         }
 
                         if (_usingTraceController)
@@ -212,7 +239,10 @@ namespace TestingPower
                             Thread.Sleep(_e3RefreshDelaySeconds * 1000);
                         }
 
-                        elevatorClient.SendControllerMessageAsync($"{Elevator.Commands.END_BROWSER} {browser}").Wait();
+                        if (!hadToCancelPass)
+                        {
+                            elevatorClient.SendControllerMessageAsync($"{Elevator.Commands.END_BROWSER} {browser}").Wait();
+                        }
                     }
                 }
                 Console.WriteLine("[{0}] - Ending Test Pass -", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
@@ -221,3 +251,4 @@ namespace TestingPower
         }
     }
 }
+;
