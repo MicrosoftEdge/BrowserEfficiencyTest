6d6418c31c6a96bbc3102ed5afce890a89fe8657
Chris Hammer <ChammerMSFT@Outlook.com>
2016-10-21 14:38:31 -0700

    Changed RefSet metric calculation to do average reference set size as well as peak for the entire system.

--- PerfProcessor/MeasureSets/RefSet.cs
+++ PerfProcessor/MeasureSets/RefSet.cs
@@ -43,29 +43,152 @@ namespace BrowserEfficiencyTest
             WprProfile = "refSet";
             TracingMode = TraceCaptureMode.File;
             Name = "refSet";
-            _wpaExportedDataFileNames = new List<string>() { "Reference_Set_RefSet_without_PagePool.csv" };
+            _wpaExportedDataFileNames = new List<string>() { "Reference_Set_RawData_without_PagePool.csv" };
         }
 
         /// <summary>
-        /// Calculates the peak memory refset in bytes for the entire system during the trace session.
+        /// Calculates the peak and average reference set size in bytes for the entire system during the trace session.
         /// </summary>
         /// <param name="csvData">The raw csv data to use for calculating the number of bytes for dynamic and file reference sets.</param>
-        /// <returns>A dictionary of the number of bytes used per refset category class.</returns>
+        /// <returns>A dictionary of processes and the number of bytes used by reference set category.</returns>
         protected override Dictionary<string, string> CalculateMetrics(Dictionary<string, List<string>> csvData)
         {
             Dictionary<string, string> metrics = null;
+            long dynamicPeakRefSet = 0;
+            long filePeakRefSet = 0;
+            long systemPeakRefSet = 0;
+            decimal dynamicPeakRefSetTimeMark = 0;
+            decimal filePeakRefSetTimeMark = 0;
+            decimal systemPeakRefSetTimeMark = 0;
+            long dynamicRunningRefSetTotal = 0;
+            long fileRunningRefSetTotal = 0;
+            long systemRunningRefSetTotal = 0;
+            decimal dynamicRefSetArea = 0;
+            decimal fileRefSetArea = 0;
+            decimal systemRefSetArea = 0;
+            decimal systemPreviousTimeMark = 0;
+            decimal dynamicPreviousTimeMark = 0;
+            decimal filePreviousTimeMark = 0;
+            decimal systemStartTimeMark = 0;
+            decimal dynamicStartTimeMark = 0;
+            decimal fileStartTimeMark = 0;
+            decimal averageSystemRefSet = 0;
+            decimal averageSystemDynamicRefSet = 0;
+            decimal averageSystemFileRefSet = 0;
 
-            // Process the raw string data into a usable format.
-            var rawRefSetData = (from row in csvData.First().Value
+            var rawRefSetData = from row in csvData.First().Value
                                 let fields = SplitCsvString(row)
-                                select new { CategoryClass = fields[0], PeakSizeMark = fields[2] }).ToDictionary( k => k.CategoryClass, v => v.PeakSizeMark );
+                                select new { CategoryClass = fields[0], AccessTime = Convert.ToDecimal(fields[1]), ReleaseTime = Convert.ToDecimal(fields[2]), Size = Convert.ToInt64(fields[3]) };
 
             if (rawRefSetData.Count() == 0)
             {
                 return null;
             }
 
-            metrics = rawRefSetData.ToDictionary( k => "Peak RefSet Size (Bytes) | " + k.Key.ToString(), v => v.Value);
+            // Create a new list with three columns: TimeMark, CategoryClass, SizeDelta.
+            // TimeMark will be created by combining the AccessTime and ReleaseTime columns from rawRefSetData.
+            // CategoryClass will be the same as in rawRefSetData.
+            // SizeDelta will be the Size column from rawRefSetData with the following caveat:
+            //     - SizeDelta will be a positive value for TimeMark values that are from the AccessTime column of rawRefSetData
+            //     - SizeDelta will be a negative value for TimeMark values that are from the ReleaseTime column of rawRefSetData
+
+            // create the positive SizeDelta values for the full list.
+            var accesses = from row in rawRefSetData
+                           select new { TimeMark = row.AccessTime, CategoryClass = row.CategoryClass, SizeDelta = row.Size };
+
+            // create the negative SizeDelta values for the full list.
+            var releases = from row in rawRefSetData
+                           select new { TimeMark = row.ReleaseTime, CategoryClass = row.CategoryClass, SizeDelta = -row.Size };
+
+            // create the full list by concatenating accesses and releases together and order by TimeMark ascending.
+            var fullList = accesses.Concat(releases).OrderBy(s => s.TimeMark);
+
+            // Step through the full list adding the SizeDeltas to get the running reference set total.
+            // Also calculate the area under each running refset total (system, dynamic, file) to use later when calculating the average.
+            foreach (var row in fullList)
+            {
+                // Calculate the area of the rectangle under the systemRunningRefSetTotal curve. This will be used to calculate the average refset for all category classes.
+                systemRefSetArea += (row.TimeMark - systemPreviousTimeMark) * systemRunningRefSetTotal;
+
+                systemRunningRefSetTotal += row.SizeDelta;
+
+                // check if this is the peak and store the value and time mark if it is the largest value so far.
+                if (systemRunningRefSetTotal > systemPeakRefSet)
+                {
+                    systemPeakRefSet = systemRunningRefSetTotal;
+                    systemPeakRefSetTimeMark = row.TimeMark;
+                }
+
+                if (row.CategoryClass.Equals("Dynamic"))
+                {
+                    // Calculate the area of the rectangle under the dynamicRunningRefSetTotal curve. This will be used to calculate the average dynamic refset.
+                    dynamicRefSetArea += (row.TimeMark - dynamicPreviousTimeMark) * dynamicRunningRefSetTotal;
+
+                    dynamicRunningRefSetTotal += row.SizeDelta;
+
+                    // check if this is the peak and store the value and time mark if it is the largest value so far.
+                    if (dynamicRunningRefSetTotal > dynamicPeakRefSet)
+                    {
+                        dynamicPeakRefSet = dynamicRunningRefSetTotal;
+                        dynamicPeakRefSetTimeMark = row.TimeMark;
+                    }
+                    dynamicPreviousTimeMark = row.TimeMark;
+                }
+
+                if (row.CategoryClass.Equals("File"))
+                {
+                    // Calculate the area of the rectangle under the fileRunningRefSetTotal curve. This will be used to calculate the average file refset.
+                    fileRefSetArea += (row.TimeMark - filePreviousTimeMark) * fileRunningRefSetTotal;
+
+                    fileRunningRefSetTotal += row.SizeDelta;
+
+                    // check if this is the peak and store the value and time mark if it is the largest value so far.
+                    if (fileRunningRefSetTotal > filePeakRefSet)
+                    {
+                        filePeakRefSet = fileRunningRefSetTotal;
+                        filePeakRefSetTimeMark = row.TimeMark;
+                    }
+                    filePreviousTimeMark = row.TimeMark;
+                }
+
+                systemPreviousTimeMark = row.TimeMark;
+            }
+
+            metrics = new Dictionary<string, string>();
+
+            // calculate the values and record the metrics
+            metrics.Add("Peak Reference Set Size (Bytes) | System | Total", systemPeakRefSet.ToString());
+            metrics.Add("Peak Reference Set Time Occurrence (seconds) | System | Total", systemPeakRefSetTimeMark.ToString());
+
+            metrics.Add("Peak Reference Set Size (Bytes) | System | Dynamic", dynamicPeakRefSet.ToString());
+            metrics.Add("Peak Reference Set Time Occurrence (seconds) | System | Dynamic", dynamicPeakRefSetTimeMark.ToString());
+
+            metrics.Add("Peak Reference Set Size (Bytes) | System | File", filePeakRefSet.ToString());
+            metrics.Add("Peak Reference Set Time Occurrence (seconds) | System | File", filePeakRefSetTimeMark.ToString());
+
+            // get the last time mark values for each category we are reporting (system, Dynamic, File)
+            systemStartTimeMark = fullList.First().TimeMark;
+            dynamicStartTimeMark = fullList.Where(s => s.CategoryClass == "Dynamic").First().TimeMark;
+            fileStartTimeMark = fullList.Where(s => s.CategoryClass == "File").First().TimeMark;
+
+            if ((systemPreviousTimeMark - systemStartTimeMark) > 0)
+            {
+                averageSystemRefSet = systemRefSetArea / (systemPreviousTimeMark - systemStartTimeMark);
+            }
+
+            if ((dynamicPreviousTimeMark - dynamicStartTimeMark) > 0)
+            {
+                averageSystemDynamicRefSet = dynamicRefSetArea / (dynamicPreviousTimeMark - dynamicStartTimeMark);
+            }
+
+            if ((filePreviousTimeMark - fileStartTimeMark) > 0)
+            {
+                averageSystemFileRefSet = fileRefSetArea / (filePreviousTimeMark - fileStartTimeMark);
+            }
+
+            metrics.Add("Average Reference Set Size (Bytes) | System | Total", averageSystemRefSet.ToString());
+            metrics.Add("Average Reference Set Size (Bytes) | System | Dynamic", averageSystemDynamicRefSet.ToString());
+            metrics.Add("Average Reference Set Size (Bytes) | System | File", averageSystemFileRefSet.ToString());
 
             return metrics;
         }
