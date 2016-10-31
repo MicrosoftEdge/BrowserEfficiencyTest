88de6bf56448e776c01e8218dd62edbe97edc7e7
Brandon Heenan <brheenan@microsoft.com>
2016-10-31 11:04:00 -0700

    Added comments to CredentialManager

--- BrowserEfficiencyTest/CredentialManager.cs
+++ BrowserEfficiencyTest/CredentialManager.cs
@@ -36,12 +36,20 @@ namespace BrowserEfficiencyTest
     {
         private List<UserInfo> _logins;
 
+        /// <summary>
+        /// Creates a new CredentialManager with info from config.json
+        /// </summary>
         public CredentialManager()
         {
             string jsonText = File.ReadAllText("config.json");
             _logins = JsonConvert.DeserializeObject<List<UserInfo>>(jsonText);
         }
 
+        /// <summary>
+        /// Given the domain requested, it returns the username and password as a UserInfo object
+        /// </summary>
+        /// <param name="domain">The desired domain, matching the domain in the config.json file</param>
+        /// <returns>A UserInfo object with the desired credentials</returns>
         public UserInfo GetCredentials(string domain)
         {
             foreach (UserInfo item in _logins)
