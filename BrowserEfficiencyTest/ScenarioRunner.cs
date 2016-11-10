bcadcff326013b2d00aceebef27ed248e9d3f00f
Brandon Heenan <brheenan@microsoft.com>
2016-11-10 11:54:57 -0800

    Bug fix: Don't sleep negative duration
    When -notimeout is specified and scenario is over it's duration, don't
    attempt to sleep because it will attempt to sleep for a negative
    duration.

--- BrowserEfficiencyTest/ScenarioRunner.cs
+++ BrowserEfficiencyTest/ScenarioRunner.cs
@@ -235,10 +235,12 @@ namespace BrowserEfficiencyTest
                                                 // continue to the next scenario. In that case, invalidate the run by throwing.
                                                 throw new Exception(string.Format("Scenario ran longer than expected! The browser ran for {0}s. The timeout for this scenario is {1}s.", runTime.TotalSeconds, scenario.Duration));
                                             }
+                                            else if (!_overrideTimeout)
+                                            {
+                                                Thread.Sleep(timeLeft);
+                                            }
 
-                                            Console.WriteLine("[{0}] - Completed - Iteration: {1}  Browser: {2}  Scenario: {3}  MeasureSet: {4}. Scenario ran for {5} seconds.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), iteration, browser, scenario.Name, currentMeasureSet.Key, runTime.TotalSeconds);
-
-                                            Thread.Sleep(timeLeft);
+                                            Console.WriteLine("[{0}] - Completed - Iteration: {1}  Browser: {2}  Scenario: {3}  MeasureSet: {4}. Scenario ran for {5} seconds.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), iteration, browser, scenario.Name, currentMeasureSet.Key, runTime.TotalSeconds); 
                                         }
 
                                         Console.WriteLine("[{0}] - Completed Browser: {1}  Iteration: {2}  MeasureSet: {3}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), browser, iteration, currentMeasureSet.Key);
